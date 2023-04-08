18fb7eb9727afcdb10e1efef8ceef646cb8494ed
JakubHerr <kuba2c@seznam.cz>
2023-04-08 20:02:26 +0200

    Add basic login screen

--- app/src/main/java/io/github/jakubherr/todo/MainActivity.kt
+++ app/src/main/java/io/github/jakubherr/todo/MainActivity.kt
@@ -1,17 +1,36 @@
 package io.github.jakubherr.todo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import io.github.jakubherr.todo.ui.theme.TodoTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
+
         setContent {
             TodoTheme {
                 TodoApp()
             }
         }
     }
+
+    private fun test(email: String, password: String) = auth.createUserWithEmailAndPassword(email, password)
+        .addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                // Sign in success, update UI with the signed-in user's information
+                Log.d("DBG", "createUserWithEmail:success")
+            } else {
+                // If sign in fails, display a message to the user.
+                Log.w("DBG", "createUserWithEmail:failure", task.exception)
+            }
+        }
 }
